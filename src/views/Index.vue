6579e4fb1f398d3f3423c7ee4b0ec77b432a03b6
dreamzhu <dreamzhu@vip.qq.com>
2017-08-30 15:39:27 +0800

    memory leak demo

---
+++ src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+	<div> memory leak demo</div>
+</template>
+
+<script>
+
+  import Vue from 'vue';
+
+  let MyPlugin = {
+    install(Vue, options) {
+    	// do something
+    }
+  };
+  Vue.use(MyPlugin);
+  export default {
+		data() {
+			return {}
+		}
+	}
+</script>
